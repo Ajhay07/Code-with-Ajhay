7bd5153780597bfb99d99424f890d76bd91fdaca
raj <torajakumar@gmail.com>
2021-08-14 13:38:21 +0800

    fix negative number case

--- Perfect number.py
+++ Perfect number.py
@@ -1,5 +1,9 @@
 a=int(input("Enter the number:"))
 
+if (a<=0):
+    print("Negative numbers and zero are not perfect!")
+    exit()
+
 b=0
 
 for x in range(1,a):
@@ -8,5 +12,6 @@ for x in range(1,a):
 
 if a==b:
     print(a,"is a perfect number")
-elif a<0:
-    print("It's not a perfect number!")
+else:
+    print(a,"is NOT a perfect number")
+
