2dc50a7f9f13f4aae0e82d2d55bbcde7e00fb36a
Ajhay07 <86193930+Ajhay07@users.noreply.github.com>
2021-08-17 08:59:28 +0530

    driving license eligibility program

---
+++ Driving license.py
@@ -0,0 +1,8 @@
+a=input("Enter the name:")
+b=int(input("Enter the age:"))
+if b>=18:
+    print(a,"is eligible for obtaining a driving license")
+else:
+    print(a,"is not eligible for obtaining a driving license")
+
+input("Press Enter key to exit the program")
