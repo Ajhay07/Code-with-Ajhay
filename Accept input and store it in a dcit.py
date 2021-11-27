42cf9aa1036da37ecee7f042d1a12f310133f001
Ajhay07 <86193930+Ajhay07@users.noreply.github.com>
2021-11-27 09:14:22 +0530

    Add files via upload
    This program is to accept the input from the user and store it in a dictionary. This program is to store the details of a student.

---
+++ Accept input and store it in a dcit.py
@@ -0,0 +1,10 @@
+#WAP to accept the details of a student and store it in a dictionary
+#rollno,name,total mark
+st={}
+r=int(input("Enter Roll no.:"))
+n=input("Name:")
+m=int(input("Total marks:"))
+st['Rollno']=r
+st['Name']=n
+st['Totmarks']=m
+print(st)
